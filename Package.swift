8e5e4dda91d1ac84cc77b009332b6afe4762e2db
Philipp Schmid <25935690+phil1995@users.noreply.github.com>
2025-06-25 19:00:28 +0200

    w.i.p.

--- Package.swift
+++ Package.swift
@@ -12,12 +12,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.15.3"),
     ],
     targets: [
         .executableTarget(name: "App",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "Hummingbird", package: "hummingbird"),
+                .product(name: "SQLite", package: "SQLite.swift"),
             ],
             path: "Sources/App"
         ),
